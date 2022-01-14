dca3afbe9f5c38f9dc023d6eb683fbc2a8e06d59
April Larsen <hunterapril94@gmail.com>
2022-01-13 17:27:56 -0800

    added hardcoded variables

---
+++ helpers/variables.js
@@ -0,0 +1,10 @@
+const categories = ["Tools", "Kitchenware", "Furniture", "Craft Equipment", "Appliances", "Other"]
+
+const rentalStatuses = ["Available", "Pending Approval", "Rented"]
+
+const neighborhoods = [
+  "Downtown", "Kitsilano", "West End", "Yaletown", "East Vancouver", "Mount Pleasant", 
+  "West Point Grey", "Gastown", "Kerrisdale", "Shaughnessy", "Fairview", "Dunbar-Southlands", "Killarney", 
+  "Marpole", "Renfrew-Collingwood", "Strathcona", "Hastings-Sunrise", "Arbutus Ridge", "Oakridge", 
+  "Kensington"
+]
