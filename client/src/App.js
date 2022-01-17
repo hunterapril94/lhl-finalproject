94b00f30e5200ad1ad29890a05870731fe9de6d0
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-17 10:26:52 -0800

    minor changes to the front end

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,13 @@ import { useEffect, useState } from "react";
 function App() {
   const [state, setState] = useState("");
   useEffect(() => {
-    axios.get("http://localhost:8001/home").then((res) => {
-      console.log(res);
-      setState(res.data.message);
-    }).catch((err)=> console.log(err.message));
+    axios
+      .get("http://localhost:8001/home")
+      .then((res) => {
+        console.log(res);
+        setState(res.data.message);
+      })
+      .catch((err) => console.log(err.message));
   }, []);
   return (
     <div className="App">
---
+++ client/src/components/nav_bar.jsx
@@ -0,0 +1,4 @@
+import { Fragment } from "react";
+function Navbar() {
+  <></>;
+}
