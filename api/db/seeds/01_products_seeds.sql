351c1331109d5f457bba0f82c805ffc6f56adf36
April Larsen <hunterapril94@gmail.com>
2022-01-17 00:34:20 -0800

    working index page

--- api/db/seeds/01_products_seeds.sql
+++ api/db/seeds/01_products_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO products (user_id, category, name, price_per_day_cents, description, deposit_amount_cents, image)
 VALUES
-(1, 'Kitchenware', 'Cuisinart Mixer', 1500, 'cuisinart mixer, red, slightly used', 4000, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.costco.ca%2Fcuisinart-precision-master-5.5-qt.-stand-mixer.product.100498089.html&psig=AOvVaw3LwGajuYIS8KfS8GqFjC4w&ust=1642482496266000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIDWsaCCuPUCFQAAAAAdAAAAABAE'),
+(1, 'Kitchenware', 'Cuisinart Mixer', 1500, 'cuisinart mixer, red, slightly used', 4000, 'https://richmedia.ca-richimage.com/ImageDelivery/imageService?profileId=12026539&id=2072402&recipeId=496'),
 (1, 'Tools', 'Power Drill', 1000, 'Black and Decker power drill, several bits included in the rental', 3000, 'https://m.media-amazon.com/images/I/711YGwfl9rS._AC_SL1500_.jpg'),
 (3, 'Craft Equipment', 'Singer Sewing Machine', 2000, 'Singer 4423, barely has been used', 10000, 'https://m.media-amazon.com/images/I/51JFZ7U25-L._AC_SL1100_.jpg'),
 (3, 'Tools', 'Craftsman Lawn Mower', 2500, 'Craftsman Electric Push Lawn Mower Please refill tank after using', 10000, 'https://cdn.rona.ca/images/02225396_L.jpg'),
