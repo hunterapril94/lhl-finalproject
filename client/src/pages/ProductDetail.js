3ff54aee0b7ca0af7ced00a0004b805d7bf4ca16
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-17 21:03:08 -0800

    data gets to products detail page

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -6,18 +6,27 @@ const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/products/${id}`).then((product) => {
-      setProduct(product);
+    axios.get(`http://localhost:8001/api/products/${id}`).then((res) => {
+      console.log(product);
+      setProduct(res.data.product);
     });
   }, []);
+  const {
+    image,
+    name,
+    category,
+    deposit_amount_cents,
+    description,
+    price_per_day_cents,
+  } = product;
   return (
     <>
-      <img src={product.image} height="300" width="300" />
-      <h2>{product.name}</h2>
-      <h3>{product.category}</h3>
-      <h3>Price: ${product.price / 100}</h3>
-      <h3>Deposit Amount: ${product.deposit_amount / 100}</h3>
-      <h3>Description: {product.description}</h3>
+      <img src={image} height="300" width="300" />
+      <h2>{name}</h2>
+      <h3>{category}</h3>
+      <h3>Price: ${price_per_day_cents / 100}</h3>
+      <h3>Deposit Amount: ${deposit_amount_cents / 100}</h3>
+      <h3>Description: {description}</h3>
     </>
   );
 };
