3d093456635cfc6fd5bad8f18038884058973b64
Kevin Mason <krmason2020@gmail.com>
2022-01-19 13:44:31 -0800

    added basic requests route file

---
+++ api/routes/requests.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+//const isUserLoggedIn = require("./helpers/isUserLoggedIn");
+//const fakeUser = require("../fake-data/user.json");
+
+module.exports = (db) => {
+  return router;
+};
