bb48e49a443b7cdb84fa733778591d4d40c195e2
Kevin Mason <krmason2020@gmail.com>
2022-01-18 17:55:54 -0800

    added some more routes

--- client/src/components/NavMenu/constants/navMenuItems.js
+++ client/src/components/NavMenu/constants/navMenuItems.js
@@ -1,8 +1,8 @@
 export const navMenuItems = [
   { id: 0, icon: "", label: "Products", route: "/" },
   { id: 1, icon: "", label: "login", route: "login" },
-  { id: 2, icon: "", label: "somewhere", route: "somewhere" },
-  { id: 3, icon: "", label: "Authentication", route: "route" },
-  { id: 4, icon: "", label: "Authentication", route: "route" },
-  { id: 5, icon: "", label: "Authentication", route: "route" },
+  { id: 2, icon: "", label: "logout", route: "logout" },
+  { id: 3, icon: "", label: "my-product-edit", route: "my-product-edit" },
+  { id: 4, icon: "", label: "about", route: "about" },
+  { id: 5, icon: "", label: "Profile", route: "profile" },
 ];
