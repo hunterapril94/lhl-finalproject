df75ed9b97c8f282ec92e500d76364f61238f4fb
Kevin Mason <krmason2020@gmail.com>
2022-01-18 19:11:05 -0800

    added context for auth in logout page

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Logout = (props) => {
-  console.log("in log out route");
+const Logout = () => {
+  const [auth, setAuth] = useOutletContext();
   const navigate = useNavigate();
   useEffect(() => {
     axios
       .post("http://localhost:8001/api/users/logout")
       .then((res) => {
         console.log(res.data);
-        props.auth(false);
+        setAuth(false);
       })
       .then(() => {
         setTimeout(() => {
