cfd778bda019e1386e4594038d6896e7d7dfd830
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-20 15:49:55 -0800

    outgoing requests are rendering on the page

--- client/src/pages/Requests/MyRequests.js
+++ client/src/pages/Requests/MyRequests.js
@@ -10,7 +10,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
-import { Box, flexbox } from "@mui/system";
+import { Box, flexbox, typography } from "@mui/system";
 import { Button } from "@mui/material";
 
 function dayParser(startDay, endDate) {
@@ -30,15 +30,19 @@ const rows = [
 ];
 
 export default function MyRequests() {
-  const [requests, setRequests] = useState([]);
+  const [IncomingRequests, setIncomingRequests] = useState([]);
+  const [OutgoingRequests, setOutgoingRequests] = useState([]);
   const [appState, setAppState] = useOutletContext();
 
   useEffect(() => {
     axios
       .get("http://localhost:8001/api/requests/pending")
       .then((res) => {
-        console.log(res.data.pendingIncommingLendRequests);
-        setRequests(res.data.pendingIncommingLendRequests);
+        const { pendingIncommingLendRequests, pendingOutgoingBorrowRequests } =
+          res.data;
+        console.log(res.data);
+        setIncomingRequests(pendingIncommingLendRequests);
+        setOutgoingRequests(pendingOutgoingBorrowRequests);
         // console.log(requests);
       })
       .catch((err) => console.log(err.message));
@@ -53,11 +57,19 @@ export default function MyRequests() {
       >
         My Pending Requests
       </Typography>
+      <Typography
+        variant="h6"
+        component="h3"
+        sx={{ marginTop: 3, textAlign: "center" }}
+      >
+        Incoming Requests:
+      </Typography>
       <Box
         sx={{
           display: "flex",
           width: "100%",
           justifyContent: "center",
+          flexDirection: "column",
 
           marginTop: 5,
         }}
@@ -67,15 +79,16 @@ export default function MyRequests() {
             <TableHead>
               <TableRow>
                 <TableCell>Item</TableCell>
-                <TableCell align="right">Requester</TableCell>
-                <TableCell align="right">cost per day</TableCell>
-                <TableCell align="right">days requested</TableCell>
-                <TableCell align="right">total revenue</TableCell>
-                <TableCell align="right">More Details</TableCell>
+                <TableCell align="center">Contact</TableCell>
+                <TableCell align="center">cost per day</TableCell>
+                <TableCell align="center">days requested</TableCell>
+                <TableCell align="center">total revenue</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center"></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {requests.map((request) => (
+              {IncomingRequests.map((request) => (
                 <TableRow
                   key={request.id}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -83,28 +96,78 @@ export default function MyRequests() {
                   <TableCell component="th" scope="row">
                     {request.name}
                   </TableCell>
-                  <TableCell align="right">{request.calories}</TableCell>
-                  <TableCell align="right">
+                  <TableCell align="center">
+                    {request.requester_email}
+                  </TableCell>
+
+                  <TableCell align="center">
                     {request.price_per_day_cents / 100} $
                   </TableCell>
-                  <TableCell align="right">
+                  <TableCell align="center">
                     {dayParser(request.start_time, request.end_time)}
                   </TableCell>
-                  <TableCell align="right">
+                  <TableCell align="center">
                     {(dayParser(request.start_time, request.end_time) *
                       request.price_per_day_cents) /
                       100}
                     $
                   </TableCell>
-                  <TableCell align="right">
-                    <Button>Details</Button>
+                  <TableCell align="center">
+                    <Button>Accept</Button>
+                    <Button>Reject</Button>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-        {/* <h1>{requests}</h1> */}
+        <Typography variant="h6" component="h3">
+          Outgoing request:
+        </Typography>
+        <TableContainer component={Paper} sx={{ width: 900 }}>
+          <Table sx={{ minWidth: 550 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Item</TableCell>
+                <TableCell align="center">Contact</TableCell>
+                <TableCell align="center">cost per day</TableCell>
+                <TableCell align="center">days requested</TableCell>
+                <TableCell align="center">total revenue</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {OutgoingRequests.map((request) => (
+                <TableRow
+                  key={request.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {request.name}
+                  </TableCell>
+                  <TableCell align="center">{request.owner_email}</TableCell>
+
+                  <TableCell align="center">
+                    {request.price_per_day_cents / 100} $
+                  </TableCell>
+                  <TableCell align="center">
+                    {dayParser(request.start_time, request.end_time)}
+                  </TableCell>
+                  <TableCell align="center">
+                    {(dayParser(request.start_time, request.end_time) *
+                      request.price_per_day_cents) /
+                      100}
+                    $
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button>Cancel</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </>
   );
