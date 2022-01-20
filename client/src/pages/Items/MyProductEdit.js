2a1eda8ed0ef2964f6216cc87b83fef53743984c
Kevin Mason <krmason2020@gmail.com>
2022-01-20 00:12:44 -0800

    removed promise all from use effect in products

--- client/src/pages/Items/MyProductEdit.js
+++ client/src/pages/Items/MyProductEdit.js
@@ -8,14 +8,13 @@ const MyProductEdit = () => {
   const [products, setProducts] = useState([]);
   const [appState, setAppState] = useOutletContext();
   useEffect(() => {
-    Promise.all([axios.get("http://localhost:8001/api/users/myproducts")])
-      .then((all) => {
+    axios
+      .get("http://localhost:8001/api/users/myproducts")
+      .then((res) => {
         // console.log(res.data.products);
-        console.log("all[0].data.products");
-        console.log(all[0].data.myProducts);
-        setProducts(all[0].data.myProducts);
+        setProducts(res.data.myProducts);
         setAppState((prev) => {
-          return { ...prev, auth: all[0].data.auth };
+          return { ...prev, auth: res.data.auth };
         });
       })
       .catch((err) => console.log(err.message));
--- client/src/pages/Items/ProductsAll.js
+++ client/src/pages/Items/ProductsAll.js
@@ -12,12 +12,13 @@ const ProductsAll = () => {
   console.log("current auth status:" + appState.auth);
 
   useEffect(() => {
-    Promise.all([axios.get("http://localhost:8001/api/products")])
-      .then((all) => {
+    axios
+      .get("http://localhost:8001/api/products")
+      .then((res) => {
         // console.log(res.data.products);
-        setProducts(all[0].data.products);
+        setProducts(res.data.products);
         setAppState((prev) => {
-          return { ...prev, auth: all[0].data.auth };
+          return { ...prev, auth: res.data.auth };
         });
       })
       .catch((err) => console.log(err.message));
