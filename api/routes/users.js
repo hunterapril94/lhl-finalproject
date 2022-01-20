78cb53f925ad526deea709f22fb472d98f2e55da
April Larsen <hunterapril94@gmail.com>
2022-01-20 15:39:58 -0800

    getting profile set up

--- api/routes/users.js
+++ api/routes/users.js
@@ -28,37 +28,37 @@ module.exports = (db) => {
   //   // /api/users/info
   //   //-----------------------------------------------------------------
 
-  //   router.get("/info", (req, res) => {
-  //     const { isLoggedIn, userID } = req; //gets this from middleware
+    router.get("/info", (req, res) => {
+      const { isLoggedIn, userID } = req; //gets this from middleware
 
-  //     if (!isLoggedIn) {
-  //       return res.json({
-  //         auth: false,
-  //         message: "not logged in",
-  //       });
-  //     }
+      if (!isLoggedIn) {
+        return res.json({
+          auth: false,
+          message: "not logged in",
+        });
+      }
 
-  //     db.getUserById(userID)
-  //       .then((userInfo) => {
-  //         return res.json({
-  //           auth: true,
-  //           message: "successful in getting user info",
-  //           userInfo,
-  //         });
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //         res.status(500).json({
-  //           auth: false,
-  //           message: "internal server error",
-  //         });
-  //       });
+      db.getUserById(userID)
+        .then((userInfo) => {
+          return res.json({
+            auth: true,
+            message: "successful in getting user info",
+            userInfo,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            auth: false,
+            message: "internal server error",
+          });
+        });
 
-  //     // return res.json({
-  //     //   auth: isLoggedIn,
-  //     //   message: "user is logged in",
-  //     // });
-  //   });
+      // return res.json({
+      //   auth: isLoggedIn,
+      //   message: "user is logged in",
+      // });
+    });
 
   //-----------------------------------------------------------------
   // GET /api/users/myBorrowedProducts
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Grid,
-  Typography,
-  ListItem,
-  List,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
   styled,
   Divider,
+  Card,
+  CardMedia,
+  CardContent,
+  Button,
+  CardHeader,
+  Box
 } from "@mui/material";
+import { useNavigate, useOutletContext } from "react-router";
 import EditIcon from "@mui/icons-material/Edit";
 import { Link, useParams } from "react-router-dom";
 import theme from "../../components/styles";
@@ -16,12 +24,22 @@ import PendingActionsIcon from "@mui/icons-material/PendingActions";
 import Inventory2Icon from "@mui/icons-material/Inventory2";
 import InfoIcon from "@mui/icons-material/Info";
 
+axios.defaults.withCredentials = true;
+
 function UserDetail() {
   const { id } = useParams;
   const [user, setUser] = useState({});
+  const [borrowed, setBorrowed] = useState();
+  const [lent, setLent] = useState()
+  const [appState, setAppState] = useOutletContext();
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/users/info`).then((user) => {
-      setUser(user);
+    const promise1 = axios.get(`http://localhost:8001/api/users/info`)
+    const promise2 = axios.get(`http://localhost:8001/api/users/myBorrowedProducts`)
+    // const promise3 = axios.get(`http://localhost:8001/api/users/myLentProducts`)
+    Promise.all([promise1, promise2])
+    .then((res) => {
+      console.log(res.data[1])
+      setUser(res.data[0].userInfo);
     });
   }, []);
   const Demo = styled("div")(({ theme }) => ({
@@ -29,71 +47,33 @@ function UserDetail() {
     color: theme.palette.tertiary.main,
   }));
   return (
-    <Grid container direction="column">
-      <Grid item xs={12} md={6}>
-        <Grid color={theme.palette.primary.main}>
-          <h1>Profile</h1>
-        </Grid>
-        <Demo>
-          <List>
-            <Link to="/profile/view">
-              <ListItem>
-                <Grid color={theme.palette.primary.main}>
-                  <PageviewIcon />
-                  View
-                </Grid>
-              </ListItem>
-            </Link>
-            <Divider />
-            <Link to="/profile/edit">
-              <ListItem>
-                <Grid color={theme.palette.primary.main}>
-                  <EditIcon />
-                  Edit
-                </Grid>
-              </ListItem>
-            </Link>
-            <Divider />
-            <Link to="/profile/pending-requests">
-              <ListItem>
-                <Grid color={theme.palette.primary.main}>
-                  <PendingActionsIcon />
-                  Pending Requests
-                </Grid>
-              </ListItem>
-            </Link>
-            <Divider />
-            <Link to="/profile/myproducts">
-              <ListItem>
-                <Grid color={theme.palette.primary.main}>
-                  <Inventory2Icon />
-                  My Products
-                </Grid>
-              </ListItem>
-            </Link>
-            <Divider />
-            <Link to="/profile/myproducts/:id">
-              <ListItem>
-                <Grid color={theme.palette.primary.main}>
-                  <InfoIcon />
-                  Product Detail
-                </Grid>
-              </ListItem>
-            </Link>
-            <Divider />
-            <Link to="/profile/myproducts/:id/edit">
-              <ListItem color={theme.palette.tertiary.main}>
-                <Grid color={theme.palette.primary.main}>
-                  <EditIcon />
-                  Product Edit
-                </Grid>
-              </ListItem>
-            </Link>
-          </List>
-        </Demo>
-      </Grid>
-      <p> {user.name}</p>
-      <p>{user.email}</p>
+    <Grid color={theme.palette.primary.main}>
+        <h1>User Profile</h1>
+        <Box display='flex' direction='row'>
+        <Grid backgroundColor="white" color={theme.palette.primary.main} xs='3'>
+            <CardMedia 
+            component='img'
+            image='https://www.pngitem.com/pimgs/m/294-2947257_interface-icons-user-avatar-profile-user-avatar-png.png'
+
+            />
+            <CardContent>
+              <h2>{user.first_name} {user.last_name}</h2>
+              <h2>{user.email}</h2>
+              <h2>Neighborhood:</h2>
+              <h2>{user.neighborhood}</h2>
+              <h2>Phone: {user.phone}</h2>
+              <h2>Borrower: {user.borrower ? "yes" : "no"}</h2>
+              <h2>Lender: {user.lender ? "yes" : "no"}</h2>
+              <Grid marginLeft='180px' container><Button variant='contained'><EditIcon /></Button></Grid>
+            </CardContent>
+          </Grid>
+          <Grid color={theme.palette.primary.main} backgroundColor={theme.palette.secondary.main} xs='9' display='flex'>
+            <h2>Borrowed Items </h2><h2><Button variant='contained' sx={{borderRadius: '50%'}}>4</Button></h2>
+          </Grid>
+
+
+        </Box>
+          
     </Grid>
   );
 }
