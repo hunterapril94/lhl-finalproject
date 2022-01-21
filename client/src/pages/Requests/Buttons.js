8c6afe86f5c76ea5018d31f843c9bc9127203c08
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-20 21:34:17 -0800

    my-requests have dates now

--- client/src/pages/Requests/Buttons.js
+++ client/src/pages/Requests/Buttons.js
@@ -66,7 +66,7 @@ export const RejectButton = (props) => {
     console.log("from inside of handle Submit");
     axios
       .post(
-        `http://localhost:8001/api/requests/${props.request.products_transactions_id}/cancel`
+        `http://localhost:8001/api/requests/${props.request.products_transactions_id}/delete`
       )
       .then((res) => props.setIncomingRequests(newIncomingRequests))
       .catch((err) => {
@@ -94,7 +94,7 @@ export const CancelButton = (props) => {
     console.log("from inside of handle Submit");
     axios
       .post(
-        `http://localhost:8001/api/requests/${props.request.products_transactions_id}/cancel`
+        `http://localhost:8001/api/requests/${props.request.products_transactions_id}/delete`
       )
       .then((res) => props.setIncomingRequests(newIncomingRequests))
       .catch((err) => {
