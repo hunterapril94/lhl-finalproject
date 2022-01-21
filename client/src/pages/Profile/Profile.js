b3081494d9d582d79de099b962f40bf2c38af1a4
April Larsen <hunterapril94@gmail.com>
2022-01-20 16:33:30 -0800

    Profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -13,7 +13,8 @@ import {
   CardContent,
   Button,
   CardHeader,
-  Box
+  Box,
+  Typography
 } from "@mui/material";
 import { useNavigate, useOutletContext } from "react-router";
 import EditIcon from "@mui/icons-material/Edit";
@@ -29,28 +30,29 @@ axios.defaults.withCredentials = true;
 function UserDetail() {
   const { id } = useParams;
   const [user, setUser] = useState({});
-  const [borrowed, setBorrowed] = useState();
-  const [lent, setLent] = useState()
+  const [borrowed, setBorrowed] = useState([]);
+  const [lent, setLent] = useState([])
   const [appState, setAppState] = useOutletContext();
   useEffect(() => {
-    const promise1 = axios.get(`http://localhost:8001/api/users/info`)
-    const promise2 = axios.get(`http://localhost:8001/api/users/my-borrowed-products`)
-    // const promise3 = axios.get(`http://localhost:8001/api/users/myLentProducts`)
+    const promise1 = axios.get(`http://localhost:8001/api/users/profile`)
+    const promise2 = axios.get(`http://localhost:8001/api/requests/pending`)
+    // const promise3 = axios.get(`http://localhost:8001/api/users/my-lent-products`)
+    // const promise4 = axios.get(`http://localhost:8001/api/users/balance`)
     Promise.all([promise1, promise2])
     .then((res) => {
-      console.log(res.data[1])
-      setUser(res.data[0].userInfo);
+      console.log(res[0].data)
+      console.log(res[1].data)
+      // console.log(res[2].data)
+      setUser(res[0].data.userProfile);
+      setBorrowed(res[1].data.pendingOutgoingBorrowRequests)
+      setLent(res[1].data.pendingIncommingLendRequests)
     });
   }, []);
-  const Demo = styled("div")(({ theme }) => ({
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.tertiary.main,
-  }));
   return (
     <Grid color={theme.palette.primary.main}>
         <h1>User Profile</h1>
         <Box display='flex' direction='row'>
-        <Grid backgroundColor="white" color={theme.palette.primary.main} xs='3'>
+        <Grid backgroundColor="white" color={theme.palette.primary.main} xs={3}>
             <CardMedia 
             component='img'
             image='https://www.pngitem.com/pimgs/m/294-2947257_interface-icons-user-avatar-profile-user-avatar-png.png'
@@ -64,12 +66,14 @@ function UserDetail() {
               <h2>Phone: {user.phone}</h2>
               <h2>Borrower: {user.borrower ? "yes" : "no"}</h2>
               <h2>Lender: {user.lender ? "yes" : "no"}</h2>
-              <Grid marginLeft='180px' container><Button variant='contained'><EditIcon /></Button></Grid>
+              <Grid marginLeft='70%' container><Button variant='contained'><EditIcon /></Button></Grid>
             </CardContent>
           </Grid>
-          <Grid color={theme.palette.primary.main} backgroundColor={theme.palette.secondary.main} xs='9' display='flex'>
-            <h2>Borrowed Items </h2><h2><Button variant='contained' sx={{borderRadius: '50%'}}>4</Button></h2>
+          <Grid color={theme.palette.primary.main} backgroundColor={theme.palette.secondary.main} xs={9} display='flex'>
+            <Typography variant='h2' color={theme.palette.primary.main} fontSize='20pt' marginTop='20px' marginLeft='10px'>Borrowed Items </Typography><h2><Button variant='contained' sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '40px'}}>{borrowed.length}</Button></h2>
+            <Typography color={theme.palette.primary.main} fontSize='20pt' marginTop='20px'>Lent Items </Typography><h2><Button variant='contained' sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '20px'}}>{lent.length}</Button></h2>
           </Grid>
+
 
 
         </Box>
