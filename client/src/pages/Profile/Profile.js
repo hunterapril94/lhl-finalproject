7689675f4cc3c0e6580e39afa12a701770f8d2c8
April Larsen <hunterapril94@gmail.com>
2022-01-20 20:37:20 -0800

    improvements to profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -37,28 +37,40 @@ function UserDetail() {
   const { id } = useParams;
   const [user, setUser] = useState({});
   const [borrowed, setBorrowed] = useState([]);
-  const [lent, setLent] = useState([])
+  const [lent, setLent] = useState([]);
+  const [currentLent, setCurrentLent] = useState([]);
+  const [currentBorrowed, setCurrentBorrowed] = useState([])
   const [appState, setAppState] = useOutletContext();
   const [showHideLent, setShowHideLent ] = useState('none')
   const [showHideBorrowed, setShowHideBorrowed] = useState('none')
+  const [showHideCurrentLent, setShowHideCurrentLent] = useState('none')
+  const [showHideCurrentBorrowed, setShowHideCurrentBorrowed] = useState('none')
+  let Balance = 0
   useEffect(() => {
     const promise1 = axios.get(`http://localhost:8001/api/users/profile`)
     const promise2 = axios.get(`http://localhost:8001/api/requests/pending`)
-    // const promise3 = axios.get(`http://localhost:8001/api/users/my-lent-products`)
-    // const promise4 = axios.get(`http://localhost:8001/api/users/balance`)
-    Promise.all([promise1, promise2])
+    const promise3 = axios.get(`http://localhost:8001/api/users/my-lent-products`)
+    const promise4 = axios.get(`http://localhost:8001/api/users/balance`)
+    const promise5 = axios.get(`http://localhost:8001/api/users/my-borrowed-products`)
+    Promise.all([promise1, promise2, promise3, promise4, promise5])
     .then((res) => {
-      console.log(res[0].data)
-      console.log(res[1].data)
-      // console.log(res[2].data)
+      // console.log(res[0].data)
+      // console.log(res[1].data)
+      console.log(res[2].data)
       setUser(res[0].data.userProfile);
       setBorrowed(res[1].data.pendingOutgoingBorrowRequests)
       setLent(res[1].data.pendingIncommingLendRequests)
+      setCurrentBorrowed(res[4].data.myBorrowedProducts)
+      setCurrentLent(res[2].data.myLentProducts)
+      Balance = res[3].data.balance.cash_balance_cents
+      // console.log(res[3].data)
+      // console.log(res[4].data)
+      // console.log(res[5].data)
+      console.log(currentBorrowed)
     });
   }, []);
   function handleClickLent(event) {
     event.preventDefault()
-  var div = document.getElementById('borrowed');
   if (showHideLent === 'none') {
     setShowHideLent('block');
   }
@@ -70,12 +82,32 @@ function UserDetail() {
 
   function handleClickBorrowed(event) {
     event.preventDefault()
-  var div = document.getElementById('borrowed');
   if (showHideBorrowed === 'none') {
     setShowHideBorrowed('block');
   }
   else {
     setShowHideBorrowed('none');
+  } 
+  }
+
+  function handleClickCurrentBorrowed(event) {
+    event.preventDefault()
+  if (showHideCurrentBorrowed === 'none') {
+    setShowHideCurrentBorrowed('block');
+  }
+  else {
+    setShowHideCurrentBorrowed('none');
+  } 
+  }
+
+  function handleClickCurrentLent(event) {
+    event.preventDefault()
+  var div = document.getElementById('borrowed');
+  if (showHideCurrentLent === 'none') {
+    setShowHideCurrentLent('block');
+  }
+  else {
+    setShowHideCurrentLent('none');
   } 
   }
   return (
@@ -101,7 +133,19 @@ function UserDetail() {
           </Grid>
           <Grid color={theme.palette.primary.main} backgroundColor={theme.palette.secondary.main} xs={9} display='flex' direction='column'>
             <Box component='form' onSubmit={handleClickLent}>
-            <Typography color={theme.palette.primary.main} fontSize='20pt' marginTop='20px' marginLeft='10px'>Lent Items </Typography><h2><Button variant='contained' sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '20px'}} type='submit'>{lent.length}</Button></h2>
+            <Typography 
+            color={theme.palette.primary.main} 
+            fontSize='20pt' 
+            marginTop='20px'
+            marginLeft='10px'>
+              Pending Lending Approval 
+            </Typography>
+            <h2><Button 
+            variant='contained' 
+            sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '20px'}} 
+            type='submit'>
+              {lent.length}
+              </Button></h2>
               <Grid display={showHideLent}>
             <Table sx={{ minWidth: 550 }} aria-label="simple table" id='lent'>
             <TableHead>
@@ -156,8 +200,20 @@ function UserDetail() {
             </Box>
             <Box component='form' onSubmit={handleClickBorrowed}>
 
-            <Typography variant='h2' color={theme.palette.primary.main} fontSize='20pt' marginTop='20px' marginLeft='10px'>Borrowed Items </Typography><h2>
-            <Button variant='contained' sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '40px'}} type='submit'>{borrowed.length}</Button></h2>
+            <Typography 
+            variant='h2' 
+            color={theme.palette.primary.main} 
+            fontSize='20pt' 
+            marginTop='20px' 
+            marginLeft='10px'>
+              Pending approval to borrow 
+            </Typography><h2>
+            <Button 
+            variant='contained' 
+            sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '40px'}} 
+            type='submit'>
+              {borrowed.length}
+              </Button></h2>
             <Grid display={showHideBorrowed}>
           <Table sx={{ minWidth: 550, display: {showHideBorrowed} }} aria-label="simple table" id='borrow'>
             <TableHead>
@@ -204,8 +260,121 @@ function UserDetail() {
           </Table>
             </Grid>
             </Box>
+            <Grid color={theme.palette.primary.main} backgroundColor={theme.palette.secondary.main} xs={9} display='flex' direction='column'>
+            <Box component='form' onSubmit={handleClickCurrentLent}>
+            <Typography color={theme.palette.primary.main} fontSize='20pt' marginTop='20px' marginLeft='10px'>Currently Lent </Typography><h2><Button variant='contained' sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '20px'}} type='submit'>{currentLent.length}</Button></h2></Box>
+            <Grid display={showHideCurrentLent}>
+            <Table sx={{ minWidth: 550 }} aria-label="simple table" id='lent'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Item</TableCell>
+                <TableCell align="center">Contact</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center">days requested</TableCell>
+                <TableCell align="center">Mark Returned</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {currentLent.map((request) => (
+                <TableRow
+                  key={request.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {request.name}
+                  </TableCell>
+                  <TableCell align="center">
+                    {request.borrower_email}
+                  </TableCell>
+
+                  <TableCell align="center">
+                  </TableCell>
+                  <TableCell align="center">
+                    {dayParser(request.start_time, request.end_time)}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Button variant='contained'>Returned</Button>
+                    
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
             
-          
+          </Table>          
+              </Grid>
+            
+            <Box component='form'
+            onSubmit={handleClickCurrentBorrowed}>
+            <Typography color={theme.palette.primary.main} 
+            fontSize='20pt' 
+            marginTop='20px' 
+            marginLeft='10px'>
+              Currently Borrowed 
+              </Typography>
+              <h2><Button 
+              variant='contained' 
+              sx={{borderRadius: '50%', marginLeft: '10px', marginRight: '20px'}} 
+              type='submit'>{
+              currentBorrowed.length}
+              </Button></h2>
+              </Box>
+            <Grid display={showHideCurrentBorrowed}>
+          <Table sx={{ minWidth: 550, display: {showHideCurrentBorrowed} }} aria-label="simple table" id='borrow'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Item</TableCell>
+                <TableCell align="center">Contact</TableCell>
+                <TableCell align="center">cost per day</TableCell>
+                <TableCell align="center">days requested</TableCell>
+                <TableCell align="center">total revenue</TableCell>
+                <TableCell align="center"></TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {currentBorrowed.map((request) => (
+                <TableRow
+                  // key={request.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {request.name}
+                  </TableCell>
+                  <TableCell align="center">{request.owner_email}</TableCell>
+
+                  <TableCell align="center">
+                    ${request.price_per_day_cents / 100}
+                  </TableCell>
+                  <TableCell align="center">
+                    {dayParser(request.start_time, request.end_time)}
+                  </TableCell>
+                  <TableCell align="center">
+                    ${(dayParser(request.start_time, request.end_time) *
+                      request.price_per_day_cents) /
+                      100}
+                    
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+            </Grid>
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            <Typography color={theme.palette.primary.main} fontSize='20pt' marginTop='20px' marginLeft='10px'>Balance: ${Balance}</Typography>
+          </Grid>
+            
           </Grid>
 
         </Box>
