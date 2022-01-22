8714e068c52c0cd5862aacc5105a0191849b187a
Kevin Mason <krmason2020@gmail.com>
2022-01-22 12:30:13 -0800

    added new seeds to products db

--- api/db/seeds/01_products_seeds.sql
+++ api/db/seeds/01_products_seeds.sql
@@ -1,12 +1,27 @@
 INSERT INTO products (user_id, category, name, price_per_day_cents, description, deposit_amount_cents, image)
 VALUES
-(1, 'Kitchenware', 'Cuisinart Mixer', 1500, 'cuisinart mixer, red, slightly used', 4000, 'https://richmedia.ca-richimage.com/ImageDelivery/imageService?profileId=12026539&id=2072402&recipeId=496'),
-(1, 'Tools', 'Power Drill', 1000, 'Black and Decker power drill, several bits included in the rental', 3000, 'https://m.media-amazon.com/images/I/711YGwfl9rS._AC_SL1500_.jpg'),
-(1, 'Tools', 'Nail Gun', 3000, 'DeWalt cordless Nail gun, useful for many different sized nails gently used', 10000, 'https://homedepot.scene7.com/is/image/homedepotcanada/p_1000851792.jpg?wid=1000&hei=1000&op_sharpen=1&product-images=l'),
-(1, 'Tools', 'Orbital Sander', 2000, 'Dewalt Orbital Sander, great for wood projects. Wireless.', 4000, 'https://images.thdstatic.com/productImages/76114bf4-f509-486c-b882-b225b278db00/svn/dewalt-disc-orbital-sanders-dcw210b-64_1000.jpg'),
-(3, 'Craft Equipment', 'Singer Sewing Machine', 2000, 'Singer 4423, barely has been used', 10000, 'https://m.media-amazon.com/images/I/51JFZ7U25-L._AC_SL1100_.jpg'),
-(3, 'Tools', 'Craftsman Lawn Mower', 2500, 'Craftsman Electric Push Lawn Mower Please refill tank after using', 10000, 'https://cdn.rona.ca/images/02225396_L.jpg'),
-(4, 'Furniture', '10 folding chairs', 1000, '10 metal folding chairs available in good condition and sturdy', 2000, 'https://images.thdstatic.com/productImages/e7870b4a-02d0-456e-b673-df6d310fe11e/svn/beige-folding-chairs-sc004x001a-64_1000.jpg'),
-(4, 'Furniture', 'Folding table', 1500, '6 foot folding table with handle for easy carry', 2000, 'https://i5.walmartimages.ca/images/Enlarge/797/016/6000196797016.jpg'),
-(6, 'Other', '4 person tent', 1000, 'roomie 4 person tent with a rain flap and tarp underneath', 3000, 'https://s7d1.scene7.com/is/image/Coleman/2000035609_24?wid=1000&hei=1000'),
-(6, 'Other', 'Propane Camping Grill', 2000, 'Propane burning small camping stove. Has a lid for easy storage and transportation', 4000, 'https://cdn.shopify.com/s/files/1/0062/4374/5910/products/P329285_ProfileDual_1_480x480.jpg?v=1607985293' );
+(1, 'Kitchenware', 'Cuisinart Mixer', 1500, 'cuisinart mixer, red, slightly used, send a message to arrange', 4000, 'https://images.unsplash.com/photo-1561677978-583a8c7a4b43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hhaXJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'),
+(1, 'Tools', 'Power Drill', 1000, 'Black and Decker power drill, several bits included in the rental', 3000, 'https://images.unsplash.com/photo-1579197586491-aed481d3a945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c2hvdmVsfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
+(1, 'Tools', 'Nail Gun', 3000, 'DeWalt cordless Nail gun, useful for many different sized nails gently used', 10000, 'https://images.unsplash.com/photo-1546827209-a218e99fdbe9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRyaWxsfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60'),
+(1, 'Tools', 'Orbital Sander', 2000, 'Dewalt Orbital Sander, great for wood projects. Wireless.', 4000, 'https://images.unsplash.com/photo-1486286701208-1d58e9338013?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c29jY2VyJTIwYmFsbGx8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'),
+(3, 'Craft Equipment', 'Singer Sewing Machine', 2000, 'Singer 4423, barely has been used, in great condition, ready today', 10000, 'https://images.unsplash.com/photo-1610048170798-85c540492ce4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzh8fHNld2luZ3xlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'),
+(3, 'Tools', 'Craftsman Lawn Mower', 2500, 'Craftsman Electric Push Lawn Mower Please refill tank after using', 10000, 'https://images.unsplash.com/photo-1607639694361-fe08243bf70c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60'),
+(4, 'Furniture', '10 folding chairs', 1000, '10 metal folding chairs available in good condition and sturdy', 2000, 'https://images.unsplash.com/photo-1511385348-a52b4a160dc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fG1hY3xlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'),
+(4, 'Furniture', 'Folding table', 1500, '6 foot folding table with handle for easy carry, ready for pick up', 2000, 'https://images.unsplash.com/photo-1617802690992-15d93263d3a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8dnJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'),
+(6, 'Other', '4 person tent', 1000, 'roomie 4 person tent with a rain flap and tarp underneath, to keep out of the rain', 3000, 'https://images.unsplash.com/photo-1606841837239-c5a1a4a07af7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YWlycG9kc3xlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'),
+(6, 'Other', 'Propane Camping Grill', 2000, 'Propane burning small camping stove. Has a lid for easy storage and transportation', 4000, 'https://images.unsplash.com/photo-1615266876483-07e781e7befe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoYWlyc3xlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60' );
+
+
+-- INSERT INTO products (user_id, category, name, price_per_day_cents, description, deposit_amount_cents, image)
+-- VALUES
+-- (1, 'Kitchenware', 'Cuisinart Mixer', 1500, 'cuisinart mixer, red, slightly used', 4000, 'https://richmedia.ca-richimage.com/ImageDelivery/imageService?profileId=12026539&id=2072402&recipeId=496'),
+-- (1, 'Tools', 'Power Drill', 1000, 'Black and Decker power drill, several bits included in the rental', 3000, 'https://m.media-amazon.com/images/I/711YGwfl9rS._AC_SL1500_.jpg'),
+-- (1, 'Tools', 'Nail Gun', 3000, 'DeWalt cordless Nail gun, useful for many different sized nails gently used', 10000, 'https://homedepot.scene7.com/is/image/homedepotcanada/p_1000851792.jpg?wid=1000&hei=1000&op_sharpen=1&product-images=l'),
+-- (1, 'Tools', 'Orbital Sander', 2000, 'Dewalt Orbital Sander, great for wood projects. Wireless.', 4000, 'https://images.thdstatic.com/productImages/76114bf4-f509-486c-b882-b225b278db00/svn/dewalt-disc-orbital-sanders-dcw210b-64_1000.jpg'),
+-- (3, 'Craft Equipment', 'Singer Sewing Machine', 2000, 'Singer 4423, barely has been used', 10000, 'https://m.media-amazon.com/images/I/51JFZ7U25-L._AC_SL1100_.jpg'),
+-- (3, 'Tools', 'Craftsman Lawn Mower', 2500, 'Craftsman Electric Push Lawn Mower Please refill tank after using', 10000, 'https://cdn.rona.ca/images/02225396_L.jpg'),
+-- (4, 'Furniture', '10 folding chairs', 1000, '10 metal folding chairs available in good condition and sturdy', 2000, 'https://images.thdstatic.com/productImages/e7870b4a-02d0-456e-b673-df6d310fe11e/svn/beige-folding-chairs-sc004x001a-64_1000.jpg'),
+-- (4, 'Furniture', 'Folding table', 1500, '6 foot folding table with handle for easy carry', 2000, 'https://i5.walmartimages.ca/images/Enlarge/797/016/6000196797016.jpg'),
+-- (6, 'Other', '4 person tent', 1000, 'roomie 4 person tent with a rain flap and tarp underneath', 3000, 'https://s7d1.scene7.com/is/image/Coleman/2000035609_24?wid=1000&hei=1000'),
+-- (6, 'Other', 'Propane Camping Grill', 2000, 'Propane burning small camping stove. Has a lid for easy storage and transportation', 4000, 'https://cdn.shopify.com/s/files/1/0062/4374/5910/products/P329285_ProfileDual_1_480x480.jpg?v=1607985293' );
+
