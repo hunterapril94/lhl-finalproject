b1cc6e048f8a907c674eb7bd9724353a06d7488c
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-01-21 18:03:12 -0800

    .env variables are added

--- api/textMessage/voyage.js
+++ api/textMessage/voyage.js
@@ -2,20 +2,13 @@ const Vonage = require("@vonage/server-sdk");
 
 require("dotenv").config();
 
+const voageNumber = process.env.VOYAGE_NUMBER;
+const toNumber = process.env.TO_NUMBER;
 const sendText = (from, to, text) => {
   const vonage = new Vonage({
     apiKey: process.env.VOYAGE_API_KEY,
     apiSecret: process.env.VOYAGE_API_SECRET,
   });
-  // const from = process.env.VOYAGE_NUMBER;
-  // const from = "18339223665";
-  // const to = "12064584774";
-  // const text = "A text message sent using the Vonage SMS API";
-  console.log(
-    process.env.VOYAGE_API_KEY,
-    process.env.VOYAGE_API_SECRET,
-    process.env.DB_HOST
-  );
 
   vonage.message.sendSms(from, to, text, (err, responseData) => {
     if (err) {
@@ -32,6 +25,6 @@ const sendText = (from, to, text) => {
   });
 };
 
-sendText(18339223665, 12064584774, "testing From voyage");
+// sendText(voageNumber, toNumber, "testing From voyage");
 
 module.export = { sendText };
