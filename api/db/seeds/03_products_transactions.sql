3956f3588f65436dc29218d4beaf48b19308c315
Kevin Mason <krmason2020@gmail.com>
2022-01-22 19:39:17 -0800

    removed all transactions from seeds

--- api/db/seeds/03_products_transactions.sql
+++ api/db/seeds/03_products_transactions.sql
@@ -1,15 +1,15 @@
-INSERT INTO products_transactions (transaction_id, product_id, start_time, end_time, status)
-VALUES
+-- INSERT INTO products_transactions (transaction_id, product_id, start_time, end_time, status)
+-- VALUES
 -- (1, 5, 'February 1, 2022', 'February 4, 2022', 'pending'),
-(1, 6, 'February 1, 2022', 'February 4, 2022', 'approved'),
-(2, 1, 'March 15, 2022', 'March 16, 2022', 'approved'),
-(3, 4, 'January 14, 2022', 'January 15, 2022', 'returned'),
-(4, 9, 'January 2, 2022', 'January 5, 2022', 'rejected'),
-(4, 10, 'January 2, 2022', 'January 5, 2022', 'canceled'),
-(5, 5, 'March 17, 2022', 'March 18, 2022', 'pending'),
-(6, 4, 'January 28, 2022', 'January 31, 2022', 'approved'),
-(7, 6, 'January 13, 2022', 'January 14, 2022', 'returned'),
-(8, 7, 'February 24, 2022', 'February 25, 2022', 'pending');
+-- (1, 6, 'February 1, 2022', 'February 4, 2022', 'approved'),
+-- (2, 1, 'March 15, 2022', 'March 16, 2022', 'approved'),
+-- (3, 4, 'January 14, 2022', 'January 15, 2022', 'returned'),
+-- (4, 9, 'January 2, 2022', 'January 5, 2022', 'rejected'),
+-- (4, 10, 'January 2, 2022', 'January 5, 2022', 'canceled'),
+-- (5, 5, 'March 17, 2022', 'March 18, 2022', 'pending'),
+-- (6, 4, 'January 28, 2022', 'January 31, 2022', 'approved'),
+-- (7, 6, 'January 13, 2022', 'January 14, 2022', 'returned'),
+-- (8, 7, 'February 24, 2022', 'February 25, 2022', 'pending');
 -- (8,8, 'February 24, 2022', 'February 25, 2022', 'pending'),
 -- (3,1, 'February 12, 2022', 'February 15, 2022', 'pending'),
 -- (3,2, 'March 12, 2022', 'March 15, 2022', 'rejected'),
