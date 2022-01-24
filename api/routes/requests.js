896c222d5dd3adc47d6189de7501dae6a5fcb534
Kevin Mason <krmason2020@gmail.com>
2022-01-24 15:31:27 -0800

    now only showing messages between transactor and owner, no other user can read messages from other users

--- api/routes/requests.js
+++ api/routes/requests.js
@@ -391,7 +391,7 @@ module.exports = (db) => {
     // console.log(req.params.txID);
     const productsTxId = req.params.txID;
 
-    db.getIncommingMessagesByProductTransactionID(productsTxId)
+    db.getIncommingMessagesByProductTransactionID(productsTxId, userID)
       .then((messages) => {
         return res.json({
           auth: true,
