9e93c7ec36c68ddc6f17e1bfab0e82c4b63041c3
Kevin Mason <krmason2020@gmail.com>
2022-01-25 01:53:05 -0800

    removing unused imports

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -37,7 +37,7 @@ function UserDetail() {
       sx: {
         bgcolor: stringToColor(name),
       },
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`, //throwing an error in console
     };
   }
 
