83fa64973f65876334673215caca200e2ec49a17
Kevin Mason <krmason2020@gmail.com>
2022-01-26 12:42:32 -0800

    added icons when logged out

--- client/src/components/NavMenu/constants/navMenuItems.js
+++ client/src/components/NavMenu/constants/navMenuItems.js
@@ -6,6 +6,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
 export const navMenuItems = (authStatus) => {
   if (authStatus) {
     return [
@@ -47,23 +49,9 @@ export const navMenuItems = (authStatus) => {
     ];
   } else {
     return [
-      { id: 0, icon: "", label: "Products", route: "/" },
-      { id: 1, icon: "", label: "login", route: "login" },
-      { id: 6, icon: "", label: "sign-up", route: "signup" },
+      { id: 0, icon: <SearchIcon />, label: "Products", route: "/" },
+      { id: 1, icon: <LoginIcon />, label: "login", route: "login" },
+      { id: 6, icon: <HowToRegIcon />, label: "sign-up", route: "signup" },
     ];
   }
 };
-
-// export const navMenuItems = [
-//   { id: 0, icon: "", label: "Products", route: "/" },
-//   { id: 1, icon: "", label: "login", route: "login" },
-//   { id: 2, icon: "", label: "logout", route: "logout" },
-//   { id: 3, icon: "", label: "my-product-edit", route: "my-product-edit" },
-//   { id: 4, icon: "", label: "about", route: "about" },
-//   { id: 5, icon: "", label: "Profile", route: "profile" },
-//   { id: 6, icon: "", label: "sign-up", route: "signup" },
-//   { id: 7, icon: "", label: "Completed Treansactions", route: "my-completed-transactions" },
-//   { id: 8, icon: "", label: "My Lent Items", route: "my-lent-items" }
-//   { id: 9, icon: "", label: "My Borrowed Items", route: "my-borrowed" }
-//   { id: 10, icon: "", label: "Pending Requests", route: "my-requests" },
-// ];
